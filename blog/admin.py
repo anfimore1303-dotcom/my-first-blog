f1cbf88c149e00ac6c126e61ad891aeb04f3fe13
anfimore1303-dotcom <anfimore1303@gmail.com>
2025-10-13 16:13:17 +0700

    Register Post in admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from django.contrib import admin
+from .models import Post
 
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "published_date", "author")
+    list_filter = ("published_date",)
+    search_fields = ("title", "text")
